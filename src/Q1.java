93baf515cc2ea7b93c1a12f0c25415eb2bbf82fa
j3ffhu <j3ffhu@gmail.com>
2024-12-16 17:25:49 -0800

    Update Q1.java

--- src/Q1.java
+++ src/Q1.java
@@ -1,9 +1,12 @@
- 
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Q1 {
- 
+
     public static long normalRound(long number) {
 
         int numDigits = (int) Math.log10(number);
@@ -12,7 +15,6 @@ public class Q1 {
 
     }
 
- 
     public static long round(long number, int digit) {
 
         int multiple = (int) Math.pow(10, digit);
@@ -22,7 +24,6 @@ public class Q1 {
 
     }
 
- 
     public static long chainRound(long x) {
 
         long ret = x;
@@ -35,34 +36,42 @@ public class Q1 {
         return ret;
     }
 
-    public static int delta(int N) {
-        int count = 0;
-
-        for (int x = 2; x <= N; x++) {
-            long normalRounded = normalRound(x);
-            long chainRounded = chainRound(x);
-
-            if (normalRounded != chainRounded) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
+        int count = sc.nextInt();
 
-        for (int t = 0; t < T; t++) {
+        List<Integer> nums = new ArrayList<Integer>();
 
-            int N = sc.nextInt();
+        Map<Integer, Integer> m = new HashMap<Integer, Integer>();
 
-            System.out.println(delta(N));
+        for (int t = 0; t < count; t++) {
+            int i = sc.nextInt();
+            nums.add(i);
+            m.put(i, -1);
         }
 
         sc.close();
+
+        int max = nums.stream().max(Integer::compareTo).get();
+
+        int subtotal = 0;
+
+        for (int x = 1; x < max + 1; x++) {
+
+            long normalRounded = normalRound(x);
+            long chainRounded = chainRound(x);
+
+            if (normalRounded != chainRounded) {
+                subtotal++;
+            }
+            if (nums.contains(x)) {
+                m.put(x, subtotal);
+            }
+        }
+
+        nums.forEach(e -> System.out.println(m.get(e)));
+
     }
 }
